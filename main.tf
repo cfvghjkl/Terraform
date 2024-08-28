4afa06cef9dd8d4c9e26a629e1abb4bd2fc5c66d
Sushma sagarika Velanati <123053169+cfvghjkl@users.noreply.github.com>
2024-08-28 11:17:30 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,6 +9,7 @@ terraform {
 provider azurerm {
   subscription_id = "03607167-68cc-48f2-b9c3-5d794ad5921a"
   tenant_id = "7a8eb511-5cd2-4e25-9c33-39f9c50852e1"
+  client_id = "94aacbff-c333-4145-9d5b-64ea48a3c08a"
   features {}
 }
 
