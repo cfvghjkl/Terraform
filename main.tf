d5ac167074d165ccdd448a9fecd54445c85bcfab
Sushma sagarika Velanati <123053169+cfvghjkl@users.noreply.github.com>
2024-08-28 12:06:54 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -10,12 +10,12 @@ provider azurerm {
   features {}
 }
 
-resource "azurerm_resource_group" "StorageApplication1" {
-  name     = "StorageApplication1"
+resource "azurerm_resource_group" "StorageApplication" {
+  name     = "StorageApplication"
   location = "West Europe"
 }
 
-resource "azurerm_storage_account" "storage_account1" {
+resource "azurerm_storage_account" "storage_account" {
   name                     = "terraformstore88301"
   resource_group_name      = azurerm_resource_group.StorageApplication.name
   location                 = azurerm_resource_group.StorageApplication.location
